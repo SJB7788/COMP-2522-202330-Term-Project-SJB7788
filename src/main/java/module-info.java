4f7addb92aff2d85f4343b5ca2ef2b713b665259
SJB7788 <83483363+SJB7788@users.noreply.github.com>
2023-11-02 16:34:06 -0700

    Hello World!

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.comp2522202330termprojectsjb7788 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.comp2522202330termprojectsjb7788 to javafx.fxml;
+    exports com.example.comp2522202330termprojectsjb7788;
+}
